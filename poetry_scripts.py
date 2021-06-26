66165889ae7b4a2175e57060d7c203cd198d1c94
neria <me@neria.dev>
2021-06-27 00:22:26 +0300

    Feature: poetry scripts
    poetry_scripts.py:
      - scripts for code linter (flake8), formatter (black) and type checker (mypy)
      - script for deployment
    
    pyproject.toml
      - run scripts from poetry_scripts.py

---
+++ poetry_scripts.py
@@ -0,0 +1,32 @@
+import os
+
+
+def poetry_run(cmd: str):
+    os.system(f"poetry run {cmd}")
+
+
+def deploy():
+    os.system("poetry install && poetry build && poetry publish")
+
+
+def black():
+    poetry_run("black .")
+
+
+def flake8():
+    poetry_run("flake8 .")
+
+
+def mypy():
+    poetry_run("python -m mypy envcon")
+
+
+def mypy_all():
+    poetry_run(
+        "python -m mypy envcon tests"
+        " --show-error-codes"
+        " --disallow-untyped-calls"
+        " --disallow-untyped-defs"
+        " --disallow-incomplete-defs"
+        " --disallow-untyped-decorators"
+    )
